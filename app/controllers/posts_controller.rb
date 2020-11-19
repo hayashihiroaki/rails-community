7a7f27d0683a2cc2801baf0b3d3811021b1f7070
hayashihiroaki <61njn0@gmail.com>
2020-11-19 13:32:55 +0900

    投稿機能

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,55 @@
+class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :update, :edit, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path,  notice: "投稿に成功しました。"
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to root_path,  notice: "投稿に更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @post.destroy
+      redirect_to root_path,  notice: "投稿に削除しました。"
+    else
+      redirect_to root_path,  alert: "投稿に削除できませんでした。"
+    end
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:content)
+    
+  end
+end

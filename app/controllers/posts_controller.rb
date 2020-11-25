e05853fecba03c275a26455163d721a22c0e57d4
hayashihiroaki <61njn0@gmail.com>
2020-11-25 13:53:07 +0900

    rubocop

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,23 +8,22 @@ class PostsController < ApplicationController
   end
 
   def show
-
   end
 
   def new
-    return redirect_to new_profile_path, alert: "プロフィールを登録してください。" if current_user.profile.blank?
+    return redirect_to new_profile_path, alert: 'プロフィールを登録してください。' if current_user.profile.blank?
+
     @post = Post.new
   end
 
   def edit
-
   end
 
   def create
     @post = Post.new(post_params)
     @post.user = current_user
     if @post.save
-      redirect_to root_path, notice: "投稿に成功しました。"
+      redirect_to root_path, notice: '投稿に成功しました。'
     else
       render :new
     end
@@ -32,7 +31,7 @@ class PostsController < ApplicationController
 
   def update
     if @post.update(post_params)
-      redirect_to root_path, notice: "投稿に更新しました。"
+      redirect_to root_path, notice: '投稿に更新しました。'
     else
       render :edit
     end
@@ -40,9 +39,9 @@ class PostsController < ApplicationController
 
   def destroy
     if @post.destroy
-      redirect_to root_path,  notice: "投稿に削除しました。"
+      redirect_to root_path,  notice: '投稿に削除しました。'
     else
-      redirect_to root_path,  alert: "投稿に削除できませんでした。"
+      redirect_to root_path,  alert: '投稿に削除できませんでした。'
     end
   end
 
@@ -54,10 +53,9 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:content, images: [])
-    
   end
 
   def redirect_unless_post
-    return redirect_to root_path, alert: "自分の投稿ではありません" if @post.user != current_user
+    return redirect_to root_path, alert: '自分の投稿ではありません' if @post.user != current_user
   end
 end
